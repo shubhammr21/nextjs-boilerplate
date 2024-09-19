b91d8bd7e344af34ad612c657b658580aab42931
Shubham Maurya <shubhammr21@gmail.com>
2024-09-19 22:00:31 +0530

    fix(layout): Move favicon link inside head tag
    - Enclose the favicon link within the <head> tag
    - This ensures proper rendering and adherence to HTML conventions
    
    The favicon link was previously placed outside the <head> section, which could lead to rendering issues or non-compliance with web standards. By moving it inside the <head> tag, we ensure that the favicon is correctly associated with the website and displayed as intended across different browsers and platforms.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
-      />
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
