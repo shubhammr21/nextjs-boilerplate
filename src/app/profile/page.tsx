2d5237201b12bd49410133e290a62951b294573e
Shubham Maurya <shubhammr21@gmail.com>
2024-09-24 09:08:33 +0530

    feat(profile): Add user profile page
    - Introduces a new page '/profile' to display user information
    - Utilizes `getServerSession` from `next-auth` to fetch the user session data
    - Renders a Card component with the user's name, email, and avatar (or fallback avatar)
    - Provides a centralized location for authenticated users to view their profile details
    
    This addition enhances the user experience by offering a dedicated space to view and manage personal information within the application.

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,24 @@
+import { Card, CardBody, User } from "@nextui-org/react"
+import { getServerSession } from "next-auth"
+
+import options from "@/config/auth"
+
+const Profile = async () => {
+  const session = await getServerSession(options)
+  return (
+    <Card className="mx-auto mt-4 max-w-md">
+      <CardBody>
+        <User
+          name={session?.user?.name}
+          description={session?.user?.email}
+          avatarProps={{
+            showFallback: !session?.user?.image,
+            src: session?.user?.image || ""
+          }}
+        />
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Profile
