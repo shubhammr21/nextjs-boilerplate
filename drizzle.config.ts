cbfb21c4d1d8d7c2db4b072df0ceab2a906f2a89
Shubham Maurya <shubhammr21@gmail.com>
2024-11-18 12:57:33 +0530

    feat(db): setup database schema and migrations
    Create initial database schema and migrations using Drizzle ORM. This includes:
    
    - Drizzle configuration file (`drizzle.config.ts`)
    - Database migration script (`0000_thin_marvel_zombies.sql`)
    - Tables for user accounts, sessions, and authentication providers
    
    This sets up the necessary database structure for managing user data, authentication, and sessions within the application. The migration script handles the creation of tables and relationships, ensuring data integrity and facilitating future schema changes.

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit"
+
+import { env } from "@/env/server"
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema/index.ts",
+  out: "./src/db/migrations",
+  dbCredentials: {
+    url: env.DATABASE_URL
+  }
+})
