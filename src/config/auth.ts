60ee462aab5488ec73d1f3526e02f5685db87190
Shubham Maurya <shubhammr21@gmail.com>
2024-11-18 13:10:57 +0530

    feat(auth): Integrate Drizzle ORM adapter for user management
    - Add DrizzleAdapter from `@auth/drizzle-adapter`
    - Import database instance from `@/db`
    - Configure NextAuth with DrizzleAdapter
    - Disable ESLint type error for adapter configuration
    
    Integrates the Drizzle ORM adapter into the NextAuth configuration, enabling user management and persistence using the database. This provides a more robust and scalable solution for handling user authentication data compared to the default in-memory storage approach.

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,13 +1,18 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { NextAuthOptions } from "next-auth"
 import AzureADProvider from "next-auth/providers/azure-ad"
 import GoogleProvider from "next-auth/providers/google"
 
+import db from "@/db"
 import { env } from "@/env/server"
 
 const options: NextAuthOptions = {
   pages: {
     signIn: "/"
   },
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  adapter: DrizzleAdapter(db),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
